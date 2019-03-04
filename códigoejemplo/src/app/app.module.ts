d3e22e0216d28ee8684a68cfdb828a43449de480
Juan Antonio Paredes León <43546148+JA-Paredes@users.noreply.github.com>
2019-03-04 19:03:04 +0100

    Add files via upload

---
+++ códigoejemplo/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// Añadido
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // Añadido
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
